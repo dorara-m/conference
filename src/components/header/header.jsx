cec25dd66beb7f4d9c71aa008d80011fbd4a7bb3
dotera <dotera.s@tam-tam.co.jp>
2024-01-19 18:18:49 +0900

    update: header固定

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,41 @@
-import styles from "./header.module.scss";
+import React, { useEffect, useState } from "react";
 import { menuList } from "../../utils/menuList.js";
+import styles from "./header.module.scss";
+import clsx from "clsx";
 
 export default function Header(props) {
+  const [isFixed, setIsFixed] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      // スクロール位置
+      const scrollPos = window.scrollY;
+      // ヘッダー固定する位置は、ページヘッダーの高さ - ヘッダーの高さ
+      const fixedPos =
+        document.querySelector(".js-pageHeader").offsetHeight -
+        document.querySelector("header").offsetHeight;
+      if (scrollPos >= fixedPos) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+    // スクロールイベントを監視
+    window.location.pathname !== "/" &&
+      window.addEventListener("scroll", handleScroll);
+    // コンポーネントのアンマウント時にイベントリスナーを削除
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header className={styles.header}>
+      <header
+        className={clsx({
+          [styles.header]: true,
+          [styles.fixed]: isFixed,
+        })}
+      >
         <a href="/" className={styles.logo}>
           <div className={styles.img}>
             <img src="/gyroid-logo.png" alt="" />
--- src/components/pageHeader/pageHeader.jsx
+++ src/components/pageHeader/pageHeader.jsx
@@ -2,7 +2,7 @@ import styles from "./pageHeader.module.scss";
 
 export default function pageHeader(props) {
   return (
-    <div className={styles.pageHeader}>
+    <div className={`${styles.pageHeader} js-pageHeader`}>
       <div className="container">
         <h1>{props.heading}</h1>
       </div>
