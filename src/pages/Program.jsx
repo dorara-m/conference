edcebd2596925bc278024987803dc03c39095cf4
dotera <dotera.s@tam-tam.co.jp>
2024-01-24 18:07:07 +0900

    mod: fb09

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -7,32 +7,38 @@ export default function Program() {
     <Layout>
       <Section heading="Speakers">
         <Section heading="Tutorial lectures" h3>
-          <TextList list={["Randall Kamien (UPENN)", "Stephen Hyde (ANU)"]} />
+          <TextList
+            list={[
+              "Randall Kamien (UPENN) Materials Geometry",
+              "Stephen Hyde (ANU) Triply Periodic Minimal Surfaces",
+            ]}
+          />
         </Section>
         <Section heading="Discussion Leader" h3>
-          <TextList list={["Gerd Schröder‐Turk (Murdoch)"]} />
+          <TextList
+            list={["Gerd Schröder‐Turk (Murdoch) Gyroids Everywhere"]}
+          />
         </Section>
         <Section heading="Plenary lectures" h3>
           <TextList
             list={[
-              "Ulrich Wiesner (Cornell)",
-              "Gregory Grason (UMASS)",
-              "Myfanwy Evans (Potsdam)",
-              "Matthias Saba (Fribourg)",
+              "Myfanwy Evans (Potsdam) Topology & Geometry",
+              "Gregory Grason (UMASS) Gyroid Physics",
+              "Ulrich Wiesner (Cornell) Gyroid Materials",
+              "Matthias Saba (Fribourg) Gyroid Photonics",
             ]}
           />
         </Section>
         <Section heading="Invited speakers" h3>
           <TextList
             list={[
-              "Xiangbing Zeng (Sheffield)",
-              "Takahiro Ichikawa (TUAT)",
-              "Kunio Awaga (Nagoya)",
-              "Łucja Kowlewska (Warsaw)",
-              "Vinodkumar Saranathan (Krea)",
-              "Philipp Schönhöfer (Michigan)",
-              "Justin Llandro (Sumitomo Chemical)",
-              // "Not yet confirmed. Justin Llandro (Tohoku)",
+              "Xiangbing Zeng (Sheffield) Liquid Crystals",
+              "Takahiro Ichikawa (TUAT) Proton Conduction",
+              "Kunio Awaga (Nagoya) Gyroid Molecules",
+              "Philipp Schönhöfer (Michigan) Gyroid Simulation",
+              "Justin Llandro (Sumitomo Chemical) Gyroid Magnetics",
+              "Łucja Kowlewska (Warsaw) Cell Membranes",
+              "Vinodkumar Saranathan (Krea) Gyroid Evolution",
             ]}
           />
         </Section>
