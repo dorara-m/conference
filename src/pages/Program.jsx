0454caebe61fe6fdc94458d1a6e6e18d0ea20e94
dotera <dotera.s@tam-tam.co.jp>
2024-01-26 12:14:23 +0900

    mod: fb10

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -9,36 +9,36 @@ export default function Program() {
         <Section heading="Tutorial lectures" h3>
           <TextList
             list={[
-              "Randall Kamien (UPENN) Materials Geometry",
-              "Stephen Hyde (ANU) Triply Periodic Minimal Surfaces",
+              "Randall Kamien (UPENN) - Materials Geometry",
+              "Stephen Hyde (ANU) - Triply Periodic Minimal Surfaces",
             ]}
           />
         </Section>
         <Section heading="Discussion Leader" h3>
           <TextList
-            list={["Gerd Schröder‐Turk (Murdoch) Gyroids Everywhere"]}
+            list={["Gerd Schröder‐Turk (Murdoch) - Gyroids Everywhere"]}
           />
         </Section>
         <Section heading="Plenary lectures" h3>
           <TextList
             list={[
-              "Myfanwy Evans (Potsdam) Topology & Geometry",
-              "Gregory Grason (UMASS) Gyroid Physics",
-              "Ulrich Wiesner (Cornell) Gyroid Materials",
-              "Matthias Saba (Fribourg) Gyroid Photonics",
+              "Myfanwy Evans (Potsdam) - Topology & Geometry",
+              "Gregory Grason (UMASS) - Gyroid Physics",
+              "Ulrich Wiesner (Cornell) - Gyroid Materials",
+              "Matthias Saba (Fribourg) - Gyroid Photonics",
             ]}
           />
         </Section>
         <Section heading="Invited speakers" h3>
           <TextList
             list={[
-              "Xiangbing Zeng (Sheffield) Liquid Crystals",
-              "Takahiro Ichikawa (TUAT) Proton Conduction",
-              "Kunio Awaga (Nagoya) Gyroid Molecules",
-              "Philipp Schönhöfer (Michigan) Gyroid Simulation",
-              "Justin Llandro (Sumitomo Chemical) Gyroid Magnetics",
-              "Łucja Kowlewska (Warsaw) Cell Membranes",
-              "Vinodkumar Saranathan (Krea) Gyroid Evolution",
+              "Xiangbing Zeng (Sheffield) - Liquid Crystals",
+              "Takahiro Ichikawa (TUAT) - Proton Conduction",
+              "Kunio Awaga (Nagoya) - Gyroid Molecules",
+              "Philipp Schönhöfer (Michigan) - Gyroid Simulation",
+              "Justin Llandro (Sumitomo Chemical) - Gyroid Magnetics",
+              "Łucja Kowlewska (Warsaw) - Cell Membranes",
+              "Vinodkumar Saranathan (Krea) - Gyroid Evolution",
             ]}
           />
         </Section>
